32c6ff6d5eb48bda2005e68ae136a65875d6353c
Mirfak-per <ga.hernandezl@duocuc.cl>
2024-10-30 11:48:01 -0300

    Add files via upload

---
+++ Clase 6/trabajar con matriz con numpy.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+A = np.array([[1, 2, 3],
+              [4, 5, 6]])
+#Las matrices tienen que tener np.array([[],[]]) esta forma
+ 
+B = np.array([[11, 12, 13],
+              [14, 15, 16]])
+# orden de una matriz
+print(A.shape)
+# (2, 3)
+# dos filas, tres columnas
+
+# suma y resta
+C = A + B
+print(C)
+
+#Ponderacion
+E = 2*A
+print(E)
+# [[ 2 4 6]
+# [ 8 10 12]]
+
+# transposici ́on
+F = A.T
+print(F)
